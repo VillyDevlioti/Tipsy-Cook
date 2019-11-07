eaadc2a95010da2f81e56d6d3e659a343f08a357
gabi <gabrie.rak@gmail.com>
2019-11-06 19:32:53 -0500

    fixed bug in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,126 +1,117 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyAYjn5bhv-pJ30tAvcuvax55A8tf-zM5zA",
-  authDomain: "tipsy-cook.firebaseapp.com",
-  databaseURL: "https://tipsy-cook.firebaseio.com",
-  projectId: "tipsy-cook",
-  storageBucket: "tipsy-cook.appspot.com",
-  messagingSenderId: "1032674956518",
-  appId: "1:1032674956518:web:8ed72d729a683d8eb95727",
-  measurementId: "G-936JM5P6LQ"
+	apiKey: "AIzaSyAYjn5bhv-pJ30tAvcuvax55A8tf-zM5zA",
+	authDomain: "tipsy-cook.firebaseapp.com",
+	databaseURL: "https://tipsy-cook.firebaseio.com",
+	projectId: "tipsy-cook",
+	storageBucket: "tipsy-cook.appspot.com",
+	messagingSenderId: "1032674956518",
+	appId: "1:1032674956518:web:8ed72d729a683d8eb95727",
+	measurementId: "G-936JM5P6LQ"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
-$( document ).ready(function() {
-  console.log( "ready!" );
-  console.log("this variable is located on another script",a);
-  
-  var userFirstName="";
-  var userLastName="";
-  var userEmail="";
+$(document).ready(function () {
+	console.log("ready!");
+	console.log("this variable is located on another script", a);
 
-  var firebaseConfig = {
-    apiKey: "AIzaSyAYjn5bhv-pJ30tAvcuvax55A8tf-zM5zA",
-    authDomain: "tipsy-cook.firebaseapp.com",
-    databaseURL: "https://tipsy-cook.firebaseio.com",
-    projectId: "tipsy-cook",
-    storageBucket: "tipsy-cook.appspot.com",
-    messagingSenderId: "1032674956518",
-    appId: "1:1032674956518:web:8ed72d729a683d8eb95727",
-    measurementId: "G-936JM5P6LQ"
-  };
+	var userFirstName = "";
+	var userLastName = "";
+	var userEmail = "";
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+	var firebaseConfig = {
+		apiKey: "AIzaSyAYjn5bhv-pJ30tAvcuvax55A8tf-zM5zA",
+		authDomain: "tipsy-cook.firebaseapp.com",
+		databaseURL: "https://tipsy-cook.firebaseio.com",
+		projectId: "tipsy-cook",
+		storageBucket: "tipsy-cook.appspot.com",
+		messagingSenderId: "1032674956518",
+		appId: "1:1032674956518:web:8ed72d729a683d8eb95727",
+		measurementId: "G-936JM5P6LQ"
+	};
 
-  var database = firebase.database();
+	// Initialize Firebase
+	firebase.initializeApp(firebaseConfig);
 
-  //create onclick event for user submitting user information
-  $("#submit-button").on("click", function(event) {
-    event.preventDefault();
-    // Get the input values
-    var userFirstName = $("#sign-in-first-name-input").val().trim();
-    var userLastName = $("#sign-in-last-name-input").val().trim();
-    var userEmail = $("#sign-in-email-input").val().trim();
-    var indexat = userEmail.indexOf("@"); //Index of "@" in the email field
-    var indexdot = userEmail.indexOf("."); //Index of "." in the email field
+	var database = firebase.database();
 
-    $("#submit-button").on("click", function(event) {
-        event.preventDefault();
-        // Get the input values
-        userFirstName = $("#sign-in-name-input").val().trim();
-        userLastName = $("#sign-in-last-name-input").val().trim();
-        userEmail = $("#sign-in-email-input").val().trim();
+	$("#submit-button").on("click", function (event) {
+		event.preventDefault();
+		// Get the input values
+		userFirstName = $("#sign-in-name-input").val().trim();
+		userLastName = $("#sign-in-last-name-input").val().trim();
+		userEmail = $("#sign-in-email-input").val().trim();
 
-  var database = firebase.database();
+		var database = firebase.database();
 
-  //create onclick event for user submitting user information
-  $("#submit-button").on("click", function(event) {
-    event.preventDefault();
-    // Get the input values
-    var userFirstName = $("#sign-in-first-name-input").val().trim();
-    var userLastName = $("#sign-in-last-name-input").val().trim();
-    var userEmail = $("#sign-in-email-input").val().trim();
-    var indexat = userEmail.indexOf("@"); //Index of "@" in the email field
-    var indexdot = userEmail.indexOf("."); //Index of "." in the email field
-      
-    //Validation of form. The function will display message if input field is blank or incorrect
-    if (userFirstName == "") {
-      $("#sign-in-first-name-input").focus();
-      $("#custom-feedback-1").css("display","block");
-      event.preventDefault();
-      event.stopPropagation();
-    } else if (userLastName == "") {
-      $("#sign-in-last-name-input").focus();
-      $("#custom-feedback-2").css("display","block");
-      event.preventDefault();
-      event.stopPropagation();
-    } else if (indexat < 1 || (indexdot-indexat) < 2) {
-      //alert('Please enter a valid Email Id');
-      $('#sign-in-email-input').focus();
-      $("#custom-feedback-3").css("display","block");
-      event.preventDefault();
-      event.stopPropagation();
-    } else {
-      // Log the user information
-      console.log(userFirstName);
-      console.log(userLastName);
-      console.log(userEmail);
+		//create onclick event for user submitting user information
+		$("#submit-button").on("click", function (event) {
+			event.preventDefault();
+			// Get the input values
+			var userFirstName = $("#sign-in-first-name-input").val().trim();
+			var userLastName = $("#sign-in-last-name-input").val().trim();
+			var userEmail = $("#sign-in-email-input").val().trim();
+			var indexat = userEmail.indexOf("@"); //Index of "@" in the email field
+			var indexdot = userEmail.indexOf("."); //Index of "." in the email field
 
-      // Save the new user info in Firebase.
-      database.ref('/user-data').push({
-        firstName: userFirstName,
-        lastName: userLastName,
-        email: userEmail,
-      });
+			//Validation of form. The function will display message if input field is blank or incorrect
+			if (userFirstName == "") {
+				$("#sign-in-first-name-input").focus();
+				$("#custom-feedback-1").css("display", "block");
+				event.preventDefault();
+				event.stopPropagation();
+			} else if (userLastName == "") {
+				$("#sign-in-last-name-input").focus();
+				$("#custom-feedback-2").css("display", "block");
+				event.preventDefault();
+				event.stopPropagation();
+			} else if (indexat < 1 || (indexdot - indexat) < 2) {
+				//alert('Please enter a valid Email Id');
+				$('#sign-in-email-input').focus();
+				$("#custom-feedback-3").css("display", "block");
+				event.preventDefault();
+				event.stopPropagation();
+			} else {
+				// Log the user information
+				console.log(userFirstName);
+				console.log(userLastName);
+				console.log(userEmail);
 
-      database.ref('/user-data').on("value", function(snapshot) {
-      
-        console.log(snapshot.val());
-            
-        // If any errors are experienced, log them to console.
-      }, function(errorObject) {
-          console.log("The read failed: " + errorObject.code);
-        });
-      
-      var userEmailLocal = userEmail; //local user email storage to preserve on load
-          localStorage.setItem("userEmail", userEmail);
-          
-      $(location).attr('href', 'test.html');
-        
-    };
+				// Save the new user info in Firebase.
+				database.ref('/user-data').push({
+					firstName: userFirstName,
+					lastName: userLastName,
+					email: userEmail,
+				});
 
-  });  
-  
-  //on.click event when user clicks "feeling lucky" button that will bring them to a random page.
-  $("#random").on("click", function(event) {
-    $(location).attr('href', 'test.html'); // need to create random html and fix link here!
-  });
-  
-});
-    
-    
+				database.ref('/user-data').on("value", function (snapshot) {
+
+					console.log(snapshot.val());
+
+					// If any errors are experienced, log them to console.
+				}, function (errorObject) {
+					console.log("The read failed: " + errorObject.code);
+				});
+
+				var userEmailLocal = userEmail; //local user email storage to preserve on load
+				localStorage.setItem("userEmail", userEmail);
+
+				$(location).attr('href', 'test.html');
+
+			};
+
+		});
+
+		//on.click event when user clicks "feeling lucky" button that will bring them to a random page.
+		$("#random").on("click", function (event) {
+			$(location).attr('href', 'test.html'); // need to create random html and fix link here!
+		});
+	
+
+	})
+
+})
 
 
