6b472d6b6fb113a39e4d09cb6739520341163b31
Villy Devlioti <vadevlio@gmail.com>
2019-11-08 23:24:16 -0500

    search by email returns the right key

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,7 +81,7 @@ $(document).ready(function () {
 
 	  //Call initialize Mood Test
 	  	console.log("Initializing buttons")
-    	initializeMoodTest();
+    	initializeMoodTest(userEmail);
 
     });
     
@@ -94,7 +94,7 @@ $(document).ready(function () {
 	 */
 
 //function to show test questions
-function initializeMoodTest ()
+function initializeMoodTest (userEmail)
 {
   $(".headline").remove();
   $("#carousel").css("display","block");
@@ -104,36 +104,32 @@ function initializeMoodTest ()
   $("#question-1").text("How are you feeling today?");
         newButton = $(".btn-style");
         console.log("column created");
-		//initButtons(moods);
+		initButtons(moods, userEmail);
         
 }
 
-function initButtons (arr){
+function initButtons (arr, email){
   for (var i=0; i<arr.length; i++){
-      newButton = newButton.append("<button type=\"button\" class=\"btn btn-outline-light btn-lg btn-style\" data-search=\""+arr[i]+"\">"+arr[i]+"</button>");
+      newButton = newButton.append("<button type=\"button\" class=\"btn btn-outline-light btn-lg btn-style\" id=\"mood-button\" data-search=\""+arr[i]+"\">"+arr[i]+"</button>");
       console.log("new button created");
   }
   
-  $(".btn").on("click", function(){
-      userSelection = $(this).attr("data-search");
-      console.log(userSelection);
+  $("#mood-button").on("click", function(){
+	userSelection = $(this).attr("data-search");
+	console.log(email);
 
-	  console.log("Checking if it exists!");
-/* 	  //now we need to check firebase for email in order to assign the user selection to the right user. 
-/* 		database.ref("/user-data").orderByChild("email").equalTo(userEmail).once("value",snapshot => {
-		  if (snapshot.exists()){
-				const userData = snapshot.val();
-			  console.log("exists!", userData);
-			  //go to the specific user and assign the mood
-  /* 			database.ref('/user-data/email').set({
-				  mood: userSelection  
-		  }) 
-	  }
-	  }); 
+});
 
-  }); */
+console.log(email);
 
-  });
+//now we need to check firebase for email in order to assign the user selection to the right user. 
+  let usersRef = database.ref('/user-data');
+	usersRef.orderByChild('email').equalTo(email).on("value", function(snapshot) {
+		  console.log(snapshot.val());
+		  snapshot.forEach(function(data) {
+		  console.log("data key:", data.key);
+	  });
+  }); 
 	
 
 } 
